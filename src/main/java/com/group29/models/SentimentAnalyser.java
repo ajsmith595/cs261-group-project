0f7df2d60a582bbf80401585c01ba0a5f80934c0
Owen <owenconnors1@gmail.com>
2021-03-14 12:47:08 +0000

    Simplified SA weighting to just be an average instead of weighted by length

--- src/main/java/com/group29/models/SentimentAnalyser.java
+++ src/main/java/com/group29/models/SentimentAnalyser.java
@@ -33,26 +33,22 @@ public class SentimentAnalyser {
     public float analyse(String message) {
         Annotation annotation = nlpPipeline.process(message);
         float[] results = new float[5];
-        int length = 0;
+        int count = 0;
 
         // Need to analyse each sentence separately. Not sure what the best way to combine these are, possibly straight average or select longest or most polarised sentence, currently going for average based on length however
         for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
             Tree tree = sentence.get(SentimentCoreAnnotations.SentimentAnnotatedTree.class);   // Generate sentence tree with sentiments
             SimpleMatrix mood_proportions = RNNCoreAnnotations.getPredictions(tree);        // Extract total ratings
-            String partText = sentence.toString();
 
-            // Add to totals weighted by sentence length
-            length += partText.length();
             for (int i = 0; i < 5; i++)
-                results[i] += mood_proportions.get(i, 0) * partText.length();
+                results[i] += mood_proportions.get(i, 0);
+                count++;
         }
-
-        if (length == 0) return 0f;
 
         // Total up resulting value
         float total = 0f;
         for (int i = 0; i < 5; i++) {
-            results[i] /= length;
+            results[i] /= count;
             total += results[i] * i;
         }
 
